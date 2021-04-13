494f1019de546eff4cca9b808bd3dc76a9358a3c
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-04-13 18:16:59 +0500

    completed step 11 install dotenv library then in gatsby-config.js require dotenv and config dotenv and created .env file in which created environment libraries

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
+const dotenv = require("dotenv");
+
+//if(process.env.NODE_ENV !== 'production'){
+dotenv.config();
+//}
+
+
 module.exports = {
     plugins: [
         "gatsby-plugin-typescript",
         {
             resolve: `gatsby-source-contentful`,
             options: {
-                spaceId: `374pb3ddawli`,
-                accessToken: `vZ-xiQJAa0DQy6YaW7cB5OXOnf9ICL2bum80caKqrUg`,
+                spaceId: process.env.CONTENTFUL_SPACE_ID,
+                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
             },
         },
     ],
