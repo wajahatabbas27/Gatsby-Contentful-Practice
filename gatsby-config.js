2d3625f923bd50fafb40eb67f1349921248af142
wajahataz <54248569+wajahatabbas27@users.noreply.github.com>
2021-04-13 17:49:20 +0500

    completed step 09 of learning gatsby from repo use graphql to call the data from the contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,12 @@
 module.exports = {
     plugins: [
-
-    ]
-}
+        "gatsby-plugin-typescript",
+        {
+            resolve: `gatsby-source-contentful`,
+            options: {
+                spaceId: `374pb3ddawli`,
+                accessToken: `vZ-xiQJAa0DQy6YaW7cB5OXOnf9ICL2bum80caKqrUg`,
+            },
+        },
+    ],
+};
